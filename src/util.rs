368301beb0485293d634878d4425d70fd5224774
Andrew Fuchs <andrew-fuchs@users.noreply.github.com>
2022-08-07 15:38:13 -0400

    Fixed issue with complilation when "util" feature flag enabled without "server", converted files with CRLF line endings to LF

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,3 @@
-use http::Method;
 #[cfg(feature = "tower")]
 use tower::ServiceBuilder;
 use tower_layer::Stack;
@@ -6,33 +5,16 @@ use tower_layer::Stack;
 #[cfg(feature = "server")]
 use crate::server::HttpServerMetricsLayer;
 
-#[cfg(feature = "util")]
-pub trait ServiceBuilderExt<L> {
+#[cfg(feature = "tower")]
+pub trait HttpMetricsServiceBuilderExt<L> {
     #[cfg(feature = "server")]
     fn http_server_metrics(self) -> ServiceBuilder<Stack<HttpServerMetricsLayer, L>>;
 }
 
-#[cfg(feature = "util")]
-impl<L> ServiceBuilderExt<L> for ServiceBuilder<L> {
+#[cfg(feature = "tower")]
+impl<L> HttpMetricsServiceBuilderExt<L> for ServiceBuilder<L> {
     #[cfg(feature = "server")]
     fn http_server_metrics(self) -> ServiceBuilder<Stack<HttpServerMetricsLayer, L>> {
         self.layer(HttpServerMetricsLayer::new())
-    }
-}
-
-/// Utility function that maps methods to a *limited* set of strings to
-/// prevent too many metrics time-series from being created.
-pub(crate) const fn label_from_method(method: &Method) -> &'static str {
-    match *method {
-        Method::CONNECT => "CONNECT",
-        Method::DELETE => "DELETE",
-        Method::GET => "GET",
-        Method::HEAD => "HEAD",
-        Method::OPTIONS => "OPTIONS",
-        Method::PATCH => "PATCH",
-        Method::POST => "POST",
-        Method::PUT => "PUT",
-        Method::TRACE => "TRACE",
-        _ => "",
     }
 }
